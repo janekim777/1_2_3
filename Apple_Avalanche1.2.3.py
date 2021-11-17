a5a2ebf0654ff3643335ffc31c54f301193998d9
janekim777 <91635326+janekim777@users.noreply.github.com>
2021-11-17 13:53:33 -0800

    Add files via upload

--- Apple_Avalanche1.2.3.py
+++ Apple_Avalanche1.2.3.py
@@ -1,5 +1,6 @@
+"""
 import turtle as trtl
-
+import random as rand
 
 
 
@@ -12,12 +13,23 @@ apple_image = "apple.gif" # Store the file name of your shape
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
-
-apple = trtl.Turtle()
-
 background_image ="background.gif"
 wn.setup(width=1.0, height=1.0)
 wn.addshape(background_image)
+apple = trtl.Turtle()
+
+letters = ["a","b","c","d","e","f","g","h","i","j"]
+
+apple_list = []
+apple_letters = []
+
+
+for i in range (5):
+  apple_list.append(trtl.Turtle())
+  apple_letters.append(rand.choice(letters))
+
+
+
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
@@ -30,10 +42,6 @@ def drop_apple():
   apple.penup()
   apple.goto(apple.xcor(),-170)
   apple.hideturtle()
-tree = trtl.Turtle()
-def draw_background(tree):
-  tree.shape(background_image)
-  wn.update()
 
 drawer = trtl.Turtle()
 
@@ -43,15 +51,16 @@ def draw_an_A():
   drawer.color("white")
   drawer.goto(-20,-30) 
   drawer.write("A", font=("Arial", 74, "bold")) 
+  drawer.hideturtle()
 
 #-----function calls-----
-
-draw_background(tree)
-draw_an_A()
-draw_apple(apple)
-drop_apple()
-
-wn.onkeypress(draw_an_A, "a")
-wn.listen()
-wn.mainloop()
 wn.bgpic("background.gif")
+draw_apple(apple)
+draw_an_A()
+drop_apple()
+drawer.clear()
+trtl.onkeypress(letters)
+wn.listen()
+
+wn.mainloop()
+""" 
